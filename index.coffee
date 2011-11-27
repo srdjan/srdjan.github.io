fcf182c19db3763fa4748efae2ec7adf111707f8
Srdjan <srdjan@timok.com>
2011-11-27 15:35:06 -0500

    started to display notes as they play

--- index.coffee
+++ index.coffee
@@ -2,7 +2,7 @@ class Canvas
 	constructor: (@ctx, @width, @height) ->
 
 	circle: (r, flash) ->
-		@ctx.clearRect(0, 0, @width, @height)
+		@ctx.clearRect(20, 20, @width-40, @height-40)
 		@ctx.beginPath()
 		@ctx.arc(@x, @y, r, 0, Math.PI*2, true)
 		@ctx.closePath()
@@ -19,24 +19,56 @@ class PaddleGame extends Canvas
 		@y = 150
 		@dx = 2
 		@dy = 4
+		@savedX = 0
+		@savedY = 0
+		@savedNote = ""
 
 	draw: =>
 		flash = false
-		if (@x + @dx > @width || @x + @dx < 0)
+		if (@x + @dx > @width-40)
+			@savedX = @x+20
+			@savedY = @y
 			@dx = -@dx
-			indx = Math.floor(Math.random() * 6) + 1
-	#		setTimeout(@players[indx].play, 0)
 			flash = true
 
-		if (@y + @dy > @height || @y + @dy < 0)
-			@dy = -@dy
-			indx = Math.floor(Math.random() * 6) + 1
-		#	setTimeout(@players[indx].play, 0)
+		if (@x + @dx < 30)
+			@savedX = @x-20
+			@savedY = @y
+			@dx = -@dx
 			flash = true
+
+		if (@y + @dy > @height-40)
+			@savedX = @x
+			@savedY = @y+30
+			@dy = -@dy
+			flash = true
+
+		if (@y + @dy < 30)	
+			@savedX = @x
+			@savedY = @y-20
+			@dy = -@dy
+			flash = true
+
+		if flash
+			indx = Math.floor(Math.random() * 6) + 1
+			setTimeout(@players[indx].player.play, 0)
+			@savedNote = @players[indx].note
+			setTimeout(@fillText, 0)
+			setTimeout(@clearText, 300)
 
 		@x += @dx
 		@y += @dy
 		@circle(10, flash)
+
+	fillText: =>
+		@ctx.fillStyle = "blue"
+		@ctx.fillText(@savedNote, @savedX, @savedY)
+
+	clearText: =>
+		@ctx.clearRect(0, 0, 20, @height)
+		@ctx.clearRect(0, 0, @width, 20)
+		@ctx.clearRect(@width-20, 0, 20, @height)
+		@ctx.clearRect(0, @height-20, @width, 20)
 
 class AudioPlayer
   constructor: (@sampleRate, @samplesLength, freq) ->
@@ -57,17 +89,19 @@ $ ->
 	setTimeout($('body').addClass('onload'), 5)
 
 	players = []
-	players.push(new AudioPlayer(44100, 7050, 1046.50)) #C6
-	players.push(new AudioPlayer(44100, 7050, 1174.66)) #D6
-	players.push(new AudioPlayer(44100, 7050, 1318.51)) #E6
-	players.push(new AudioPlayer(44100, 7050, 1396.91)) #F6
-	players.push(new AudioPlayer(44100, 7050, 1567.98)) #G6
-	players.push(new AudioPlayer(44100, 7050, 1760.00)) #A6
-	players.push(new AudioPlayer(44100, 7050, 1975.53)) #B6
+	players.push(	{ player: new AudioPlayer(44100, 7050, 523.26), note: "#C4"	} )
+	players.push(	{ player: new AudioPlayer(44100, 7050, 1174.66), note: "#D6"	} )
+	players.push(	{ player: new AudioPlayer(44100, 7050, 1318.51), note: "#E6"	} )
+	players.push(	{ player: new AudioPlayer(44100, 7050, 830.61), note: "#G4"	} )
+	players.push(	{ player: new AudioPlayer(44100, 7050, 932.33), note: "#A#5"	} )
+	players.push(	{ player: new AudioPlayer(44100, 7050, 987.77), note: "#B5"	} )
+	players.push(	{ player: new AudioPlayer(44100, 7050, 880), note: "#A5"	} )
 		
 	ctx = $("#canvas")[0].getContext("2d")
 	width = $("#canvas").width()
 	height = $("#canvas").height()
+	ctx.font = "bold 10pt Calibri"
+	ctx.textAlign = "center"
+	ctx.textBaseline = "middle"
 	paddleGame = new PaddleGame(ctx, width, height, players)
-
-	intervalID = setInterval(paddleGame.draw, 10)
+	intervalID = setInterval(paddleGame.draw, 15)
--- index.js
+++ index.js
@@ -15,7 +15,7 @@
       this.height = height;
     }
     Canvas.prototype.circle = function(r, flash) {
-      this.ctx.clearRect(0, 0, this.width, this.height);
+      this.ctx.clearRect(20, 20, this.width - 40, this.height - 40);
       this.ctx.beginPath();
       this.ctx.arc(this.x, this.y, r, 0, Math.PI * 2, true);
       this.ctx.closePath();
@@ -32,29 +32,65 @@
     __extends(PaddleGame, Canvas);
     function PaddleGame(ctx, width, height, players) {
       this.players = players;
+      this.clearText = __bind(this.clearText, this);
+      this.fillText = __bind(this.fillText, this);
       this.draw = __bind(this.draw, this);
       PaddleGame.__super__.constructor.call(this, ctx, width, height);
       this.x = 150;
       this.y = 150;
       this.dx = 2;
       this.dy = 4;
+      this.savedX = 0;
+      this.savedY = 0;
+      this.savedNote = "";
     }
     PaddleGame.prototype.draw = function() {
       var flash, indx;
       flash = false;
-      if (this.x + this.dx > this.width || this.x + this.dx < 0) {
+      if (this.x + this.dx > this.width - 40) {
+        this.savedX = this.x + 20;
+        this.savedY = this.y;
         this.dx = -this.dx;
-        indx = Math.floor(Math.random() * 6) + 1;
         flash = true;
       }
-      if (this.y + this.dy > this.height || this.y + this.dy < 0) {
-        this.dy = -this.dy;
-        indx = Math.floor(Math.random() * 6) + 1;
+      if (this.x + this.dx < 30) {
+        this.savedX = this.x - 20;
+        this.savedY = this.y;
+        this.dx = -this.dx;
         flash = true;
+      }
+      if (this.y + this.dy > this.height - 40) {
+        this.savedX = this.x;
+        this.savedY = this.y + 30;
+        this.dy = -this.dy;
+        flash = true;
+      }
+      if (this.y + this.dy < 30) {
+        this.savedX = this.x;
+        this.savedY = this.y - 20;
+        this.dy = -this.dy;
+        flash = true;
+      }
+      if (flash) {
+        indx = Math.floor(Math.random() * 6) + 1;
+        setTimeout(this.players[indx].player.play, 0);
+        this.savedNote = this.players[indx].note;
+        setTimeout(this.fillText, 0);
+        setTimeout(this.clearText, 300);
       }
       this.x += this.dx;
       this.y += this.dy;
       return this.circle(10, flash);
+    };
+    PaddleGame.prototype.fillText = function() {
+      this.ctx.fillStyle = "blue";
+      return this.ctx.fillText(this.savedNote, this.savedX, this.savedY);
+    };
+    PaddleGame.prototype.clearText = function() {
+      this.ctx.clearRect(0, 0, 20, this.height);
+      this.ctx.clearRect(0, 0, this.width, 20);
+      this.ctx.clearRect(this.width - 20, 0, 20, this.height);
+      return this.ctx.clearRect(0, this.height - 20, this.width, 20);
     };
     return PaddleGame;
   })();
@@ -83,17 +119,41 @@
     var ctx, height, intervalID, paddleGame, players, width;
     setTimeout($('body').addClass('onload'), 5);
     players = [];
-    players.push(new AudioPlayer(44100, 7050, 1046.50));
-    players.push(new AudioPlayer(44100, 7050, 1174.66));
-    players.push(new AudioPlayer(44100, 7050, 1318.51));
-    players.push(new AudioPlayer(44100, 7050, 1396.91));
-    players.push(new AudioPlayer(44100, 7050, 1567.98));
-    players.push(new AudioPlayer(44100, 7050, 1760.00));
-    players.push(new AudioPlayer(44100, 7050, 1975.53));
+    players.push({
+      player: new AudioPlayer(44100, 7050, 523.26),
+      note: "#C4"
+    });
+    players.push({
+      player: new AudioPlayer(44100, 7050, 1174.66),
+      note: "#D6"
+    });
+    players.push({
+      player: new AudioPlayer(44100, 7050, 1318.51),
+      note: "#E6"
+    });
+    players.push({
+      player: new AudioPlayer(44100, 7050, 830.61),
+      note: "#G4"
+    });
+    players.push({
+      player: new AudioPlayer(44100, 7050, 932.33),
+      note: "#A#5"
+    });
+    players.push({
+      player: new AudioPlayer(44100, 7050, 987.77),
+      note: "#B5"
+    });
+    players.push({
+      player: new AudioPlayer(44100, 7050, 880),
+      note: "#A5"
+    });
     ctx = $("#canvas")[0].getContext("2d");
     width = $("#canvas").width();
     height = $("#canvas").height();
+    ctx.font = "bold 10pt Calibri";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
     paddleGame = new PaddleGame(ctx, width, height, players);
-    return intervalID = setInterval(paddleGame.draw, 10);
+    return intervalID = setInterval(paddleGame.draw, 15);
   });
 }).call(this);
