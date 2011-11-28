c9ac6a19582267b282f8485aead3c8214d784ddd
Srdjan <srdjan@timok.com>
2011-11-27 21:40:59 -0500

    cleanup

--- index.coffee
+++ index.coffee
@@ -1,5 +1,12 @@
 class Canvas
-	constructor: (@ctx, @width, @height) ->
+	constructor: (@ctx, @width, @height, @players) ->
+		@x = 150
+		@y = 150
+		@dx = 2
+		@dy = 4
+		@savedX = 0
+		@savedY = 0
+		@savedNote = ""
 
 	circle: (r, flash) ->
 		@ctx.clearRect(20, 20, @width-40, @height-40)
@@ -11,17 +18,6 @@ class Canvas
 		else
 			@ctx.fillStyle = "blue"
 		@ctx.fill()
-
-class PaddleGame extends Canvas
-	constructor: (ctx, width, height, @players) ->
-		super(ctx, width, height)
-		@x = 150
-		@y = 150
-		@dx = 2
-		@dy = 4
-		@savedX = 0
-		@savedY = 0
-		@savedNote = ""
 
 	draw: =>
 		flash = false
@@ -50,11 +46,11 @@ class PaddleGame extends Canvas
 			flash = true
 
 		if flash
-			indx = Math.floor(Math.random() * 6) + 1
+			indx = @x % 7 #Math.floor(Math.random() * 6) + 1
 			setTimeout(@players[indx].player.play, 0)
 			@savedNote = @players[indx].note
 			setTimeout(@fillText, 0)
-			setTimeout(@clearText, 300)
+			setTimeout(@clearText, 500)
 
 		@x += @dx
 		@y += @dy
@@ -70,38 +66,38 @@ class PaddleGame extends Canvas
 		@ctx.clearRect(@width-20, 0, 20, @height)
 		@ctx.clearRect(0, @height-20, @width, 20)
 
-class AudioPlayer
-  constructor: (@sampleRate, @samplesLength, freq) ->
+class Note
+  constructor: (@samplesLength, freq) ->
     @audio = new Audio()
-    @audio.mozSetup(1, @sampleRate)
+    @audio.mozSetup(1, 44100)
     
     @samples = new Float32Array(@samplesLength)
-    k = 2 * Math.PI * freq / @sampleRate
+    k = 2 * Math.PI * freq / 44100
     i = 0
-    currentSoundSample = 0
     while i < @samplesLength
-      @samples[i++] = 0.5 * Math.sin(k * currentSoundSample++)
+      @samples[i++] = 0.5 * Math.sin(k * i++)
 
   play: => 
 	  @audio.mozWriteAudio(@samples)
 
 $ ->
+	players = [
+		{ player: new Note(7050, 440), note: "#A4"	},
+		{ player: new Note(7050, 493.883), note: "#B4"	},
+		{ player: new Note(7050, 523.26), note: "#C4"	},
+		{ player: new Note(7050, 830.61), note: "#G4"	},
+		{ player: new Note(7050, 880), note: "#A5"	},
+		{ player: new Note(7050, 987.77), note: "#B5"	},
+		{ player: new Note(7050, 1046.50), note: "#C6"	}
+	]
+
 	setTimeout($('body').addClass('onload'), 5)
 
-	players = []
-	players.push(	{ player: new AudioPlayer(44100, 7050, 523.26), note: "#C4"	} )
-	players.push(	{ player: new AudioPlayer(44100, 7050, 1174.66), note: "#D6"	} )
-	players.push(	{ player: new AudioPlayer(44100, 7050, 1318.51), note: "#E6"	} )
-	players.push(	{ player: new AudioPlayer(44100, 7050, 830.61), note: "#G4"	} )
-	players.push(	{ player: new AudioPlayer(44100, 7050, 932.33), note: "#A#5"	} )
-	players.push(	{ player: new AudioPlayer(44100, 7050, 987.77), note: "#B5"	} )
-	players.push(	{ player: new AudioPlayer(44100, 7050, 880), note: "#A5"	} )
-		
 	ctx = $("#canvas")[0].getContext("2d")
 	width = $("#canvas").width()
 	height = $("#canvas").height()
 	ctx.font = "bold 10pt Calibri"
 	ctx.textAlign = "center"
 	ctx.textBaseline = "middle"
-	paddleGame = new PaddleGame(ctx, width, height, players)
-	intervalID = setInterval(paddleGame.draw, 15)
+	canvas = new Canvas(ctx, width, height, players)
+	setInterval(canvas.draw, 15)
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 (function() {
-  var AudioPlayer, Canvas, PaddleGame;
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
-    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
-    function ctor() { this.constructor = child; }
-    ctor.prototype = parent.prototype;
-    child.prototype = new ctor;
-    child.__super__ = parent.prototype;
-    return child;
-  };
+  var Canvas, Note;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   Canvas = (function() {
-    function Canvas(ctx, width, height) {
+    function Canvas(ctx, width, height, players) {
       this.ctx = ctx;
       this.width = width;
       this.height = height;
+      this.players = players;
+      this.clearText = __bind(this.clearText, this);
+      this.fillText = __bind(this.fillText, this);
+      this.draw = __bind(this.draw, this);
+      this.x = 150;
+      this.y = 150;
+      this.dx = 2;
+      this.dy = 4;
+      this.savedX = 0;
+      this.savedY = 0;
+      this.savedNote = "";
     }
     Canvas.prototype.circle = function(r, flash) {
       this.ctx.clearRect(20, 20, this.width - 40, this.height - 40);
@@ -26,25 +30,7 @@
       }
       return this.ctx.fill();
     };
-    return Canvas;
-  })();
-  PaddleGame = (function() {
-    __extends(PaddleGame, Canvas);
-    function PaddleGame(ctx, width, height, players) {
-      this.players = players;
-      this.clearText = __bind(this.clearText, this);
-      this.fillText = __bind(this.fillText, this);
-      this.draw = __bind(this.draw, this);
-      PaddleGame.__super__.constructor.call(this, ctx, width, height);
-      this.x = 150;
-      this.y = 150;
-      this.dx = 2;
-      this.dy = 4;
-      this.savedX = 0;
-      this.savedY = 0;
-      this.savedNote = "";
-    }
-    PaddleGame.prototype.draw = function() {
+    Canvas.prototype.draw = function() {
       var flash, indx;
       flash = false;
       if (this.x + this.dx > this.width - 40) {
@@ -72,88 +58,81 @@
         flash = true;
       }
       if (flash) {
-        indx = Math.floor(Math.random() * 6) + 1;
+        indx = this.x % 7;
         setTimeout(this.players[indx].player.play, 0);
         this.savedNote = this.players[indx].note;
         setTimeout(this.fillText, 0);
-        setTimeout(this.clearText, 300);
+        setTimeout(this.clearText, 500);
       }
       this.x += this.dx;
       this.y += this.dy;
       return this.circle(10, flash);
     };
-    PaddleGame.prototype.fillText = function() {
+    Canvas.prototype.fillText = function() {
       this.ctx.fillStyle = "blue";
       return this.ctx.fillText(this.savedNote, this.savedX, this.savedY);
     };
-    PaddleGame.prototype.clearText = function() {
+    Canvas.prototype.clearText = function() {
       this.ctx.clearRect(0, 0, 20, this.height);
       this.ctx.clearRect(0, 0, this.width, 20);
       this.ctx.clearRect(this.width - 20, 0, 20, this.height);
       return this.ctx.clearRect(0, this.height - 20, this.width, 20);
     };
-    return PaddleGame;
+    return Canvas;
   })();
-  AudioPlayer = (function() {
-    function AudioPlayer(sampleRate, samplesLength, freq) {
-      var currentSoundSample, i, k;
-      this.sampleRate = sampleRate;
+  Note = (function() {
+    function Note(samplesLength, freq) {
+      var i, k;
       this.samplesLength = samplesLength;
       this.play = __bind(this.play, this);
       this.audio = new Audio();
-      this.audio.mozSetup(1, this.sampleRate);
+      this.audio.mozSetup(1, 44100);
       this.samples = new Float32Array(this.samplesLength);
-      k = 2 * Math.PI * freq / this.sampleRate;
+      k = 2 * Math.PI * freq / 44100;
       i = 0;
-      currentSoundSample = 0;
       while (i < this.samplesLength) {
-        this.samples[i++] = 0.5 * Math.sin(k * currentSoundSample++);
+        this.samples[i++] = 0.5 * Math.sin(k * i++);
       }
     }
-    AudioPlayer.prototype.play = function() {
+    Note.prototype.play = function() {
       return this.audio.mozWriteAudio(this.samples);
     };
-    return AudioPlayer;
+    return Note;
   })();
   $(function() {
-    var ctx, height, intervalID, paddleGame, players, width;
+    var canvas, ctx, height, players, width;
+    players = [
+      {
+        player: new Note(7050, 440),
+        note: "#A4"
+      }, {
+        player: new Note(7050, 493.883),
+        note: "#B4"
+      }, {
+        player: new Note(7050, 523.26),
+        note: "#C4"
+      }, {
+        player: new Note(7050, 830.61),
+        note: "#G4"
+      }, {
+        player: new Note(7050, 880),
+        note: "#A5"
+      }, {
+        player: new Note(7050, 987.77),
+        note: "#B5"
+      }, {
+        player: new Note(7050, 1046.50),
+        note: "#C6"
+      }
+    ];
     setTimeout($('body').addClass('onload'), 5);
-    players = [];
-    players.push({
-      player: new AudioPlayer(44100, 7050, 523.26),
-      note: "#C4"
-    });
-    players.push({
-      player: new AudioPlayer(44100, 7050, 1174.66),
-      note: "#D6"
-    });
-    players.push({
-      player: new AudioPlayer(44100, 7050, 1318.51),
-      note: "#E6"
-    });
-    players.push({
-      player: new AudioPlayer(44100, 7050, 830.61),
-      note: "#G4"
-    });
-    players.push({
-      player: new AudioPlayer(44100, 7050, 932.33),
-      note: "#A#5"
-    });
-    players.push({
-      player: new AudioPlayer(44100, 7050, 987.77),
-      note: "#B5"
-    });
-    players.push({
-      player: new AudioPlayer(44100, 7050, 880),
-      note: "#A5"
-    });
     ctx = $("#canvas")[0].getContext("2d");
     width = $("#canvas").width();
     height = $("#canvas").height();
     ctx.font = "bold 10pt Calibri";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    paddleGame = new PaddleGame(ctx, width, height, players);
-    return intervalID = setInterval(paddleGame.draw, 15);
+    canvas = new Canvas(ctx, width, height, players);
+    return setInterval(canvas.draw, 15);
   });
 }).call(this);
