c2848b18837efd97d380ed5c104805fb8682ce27
Srdjan <srdjan@timok.com>
2011-11-28 23:02:51 -0500

    added more tones

--- index.js
+++ index.js
@@ -13,16 +13,12 @@
       this.dx = 2;
       this.dy = 4;
     }
-    Canvas.prototype.circle = function(r, flash) {
+    Canvas.prototype.circle = function(r, color, x, y) {
       this.ctx.clearRect(0, 0, this.width, this.height);
       this.ctx.beginPath();
-      this.ctx.arc(this.x, this.y, r, 0, Math.PI * 2, true);
+      this.ctx.arc(x, y, r, 0, Math.PI * 2, true);
       this.ctx.closePath();
-      if (flash) {
-        this.ctx.fillStyle = "red";
-      } else {
-        this.ctx.fillStyle = "blue";
-      }
+      this.ctx.fillStyle = color;
       return this.ctx.fill();
     };
     Canvas.prototype.draw = function() {
@@ -36,21 +32,19 @@
         this.dy = -this.dy;
         flash = true;
       }
-      if (flash) {
-        indx = Math.floor(Math.random() * 5) + 1;
-        setTimeout(this.player.play, 0, indx);
-        console.log("indx: " + indx);
-      }
       this.x += this.dx;
       this.y += this.dy;
-      return this.circle(10, flash);
+      if (flash) {
+        indx = Math.floor(Math.random() * 8);
+        setTimeout(this.player.play, 0, indx);
+      }
+      return this.circle(10, "blue", this.x, this.y);
     };
     return Canvas;
   })();
   Player = (function() {
     function Player() {
-      this.play = __bind(this.play, this);      var data, i, middleC, note, params, waves;
-      middleC = 261.6260;
+      this.play = __bind(this.play, this);      var data, i, note, params, waves;
       this.notes = [];
       waves = [];
       waves[0] = ["square", 0.0000, 0.4000, 0.0000, 0.1160, 0.0000, 0.3020, 110.0000, 472.0000, 2400.0000, -0.7880, 0.0000, 0.0000, 0.0100, 0.0003, 0.0000, 0.0000, 0.0000, 0.5000, -0.1020, 0.0000, 0.0000, 0.0000, 1.0000, 0.0000, 0.0000, 0.0000, 0.0000];
@@ -59,6 +53,9 @@
       waves[3] = ["synth", 1.0000, 0.4020, 0.1530, 0.2600, 1.2750, 0.8880, 20.0000, 1022.0000, 2400.0000, 0.3800, 0.6440, 0.2130, 14.8869, 0.2330, -0.4900, -0.7120, 0.1440, 0.0960, -0.5780, 0.2256, 0.3940, -0.5140, 1.0000, -0.4180, 0.2470, 0.2260, -0.5780];
       waves[4] = ["saw", 1.0000, 0.4020, 0.2640, 0.7200, 1.5390, 0.7240, 148.0000, 449.0000, 1926.0000, -0.1120, 0.0000, 0.0000, 0.0100, 0.0003, 0.0000, 0.0000, 0.0000, 0.5000, -0.1160, 0.0000, 0.0000, 0.0000, 1.0000, 0.0000, 0.0000, 0.0000, 0.0000];
       waves[5] = ["saw", 0.0000, 0.6820, 0.2570, 0.4020, 0.5220, 0.4640, 626.0000, 84.0000, 1534.0000, 0.4600, 0.0340, 0.2490, 5.5289, 0.9272, -0.3300, -0.4100, 0.1260, 0.0400, 0.3340, 0.0000, 0.0000, 0.0000, 1.0000, 0.0000, 0.0000, 0.1000, 0.0000];
+      waves[6] = ["saw", 1.0000, 0.4020, 0.2640, 0.7200, 1.5390, 0.7240, 148.0000, 449.0000, 1926.0000, -0.1120, -0.7900, 0.4540, 8.4562, 0.2473, 0.2980, -0.6700, 0.0000, 0.0975, -0.4480, 0.0856, -0.5180, -0.4720, 1.0000, -0.4480, 0.3410, 0.8160, 1.0000];
+      waves[7] = ["square", 1.0000, 0.4020, 0.2640, 0.7200, 1.5390, 0.7240, 376.0000, 20.0000, 1926.0000, -0.1120, 0.7020, 0.4540, 8.4562, 0.2473, 0.2980, -0.6700, 0.0000, 0.0975, -0.4480, 0.0856, -0.5180, -0.4720, 1.0000, -0.4480, 0.3410, 0.8160, 1.0000];
+      waves[8] = ["sine", 1.0000, 0.4020, 0.4940, 1.0340, 2.6610, 1.8000, 376.0000, 20.0000, 1926.0000, -0.1120, 0.7020, 0.4540, 8.4562, 0.2473, 0.2980, -0.6700, 0.0000, 0.0975, -0.4480, 0.0856, -0.5180, -0.4720, 1.0000, -0.4480, 0.3410, 0.8160, 1.0000];
       i = 0;
       while (i < waves.length) {
         note = {};
@@ -70,7 +67,8 @@
       }
     }
     Player.prototype.play = function(i) {
-      return this.notes[i].data.play();
+      this.notes[i].data.play();
+      return console.log(i);
     };
     return Player;
   })();
@@ -85,6 +83,6 @@
     ctx.textBaseline = "middle";
     player = new Player;
     canvas = new Canvas(ctx, width, height, player);
-    return setInterval(canvas.draw, 15);
+    return setInterval(canvas.draw, 13);
   });
 }).call(this);
