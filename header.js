bd0642e702e1af50098e1788154d08baa37152ce
Srdjan Strbanovic <srdjan@timok.com>
2020-05-06 00:11:42 -0400

    more refactoring...

--- header.js
+++ header.js
@@ -30,28 +30,27 @@ function MyHeader() {
     const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
     const h = i => (i * 360) / cards.length
     const l = i => 100 - ((i + 1) * 100) / cards.length
-    const grey = i => hsl(0, 0, l(i))
     
     const showHeader = (i) =>
       cards.forEach((c, j) => {
         c.style.backgroundColor = hsl(h(i + j / cards.length), 100, l(j))
-        c.style.flexGrow = cards.length / 0.5
+        c.style.flexGrow = cards.length / i===j ? 0.7 : 0.3
       })
 
     const reset = () =>
       cards.forEach((c, i) => {
-        c.style.backgroundColor = grey(i)
+        c.style.backgroundColor = hsl(0, 0, l(i))
         c.style.flexGrow = 1
       })
 
-      cards.forEach((card, i) => {
-        card.addEventListener('mouseover', () => showHeader(i))
-        card.addEventListener('mouseleave', () => reset())
-      })
+    cards.forEach((card, i) => {
+      card.addEventListener('mouseover', () => showHeader(i))
+      card.addEventListener('mouseleave', () => reset())
+    })
     
-      // -- execute 
+    // execute onload: show & and hide the header
     showHeader(Math.random() % cards.length * 12)
-    setTimeout(() => reset(), 500)
+    setTimeout(() => reset(), 750)
   }, [])
 
 	return html`
