81ffdec3a02ff4dd0e897b8835d413648330ac80
Srdjan Strbanovic <srdjan@timok.com>
2020-05-11 12:20:23 -0400

    simplification

--- header.js
+++ header.js
@@ -1,29 +1,10 @@
-const init = function () {
-  let cards = document.querySelectorAll(".card")
-    
-  const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
-  const h = i => (i * 360) / cards.length
-  const l = i => 100 - ((i + 1) * 100) / cards.length
+let cards = document.querySelectorAll(".card")
   
-  const showHeader = (i) =>
-    cards.forEach((c, j) => {
-      c.style.backgroundColor = hsl(h(i + j / cards.length), 100, l(j))
-      c.style.flexGrow = i===j ? 0.8 : 0.5
-    })
+const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
+const h = i => (i * 360) / cards.length
+const l = i => 100 - ((i + 1) * 100) / cards.length
+const showHeader = (i) => cards.forEach((c, j) => c.style.backgroundColor = hsl(h(i + j / 20), 100, l(j)))
+const reset = () => cards.forEach((c, i) => c.style.backgroundColor = hsl(0, 0, l(i)))
 
-  const reset = () =>
-    cards.forEach((c, i) => {
-      c.style.backgroundColor = hsl(0, 0, l(i))
-      c.style.flexGrow = 1
-    })
-
-  cards.forEach((card, i) => {
-    card.addEventListener('mouseover', () => showHeader(Math.random() % i))
-    card.addEventListener('mouseleave', () => reset())
-  })
-  
-  showHeader(Math.random() % cards.length)
-  setTimeout(() => reset(), 2500)
-}  
-
-init()
+showHeader(Math.random() % cards.length * 3)
+setTimeout(() => reset(), 2500)
