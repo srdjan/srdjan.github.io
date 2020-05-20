3111ad1d079262ff282cf2c137fdc5747d179614
Srdjan Strbanovic <srdjan@timok.com>
2020-05-20 16:26:28 -0400

    added drop-shadow to the footer

--- header.js
+++ header.js
@@ -6,5 +6,5 @@ const l = i => 100 - ((i + 1) * 100) / cards.length
 const showHeader = (i) => cards.forEach((c, j) => c.style.backgroundColor = hsl(h(i + j / 20), 100, l(j)))
 const reset = () => cards.forEach((c, i) => c.style.backgroundColor = hsl(0, 0, l(i)))
 
-showHeader(Math.random() % cards.length * 3)
-setTimeout(() => reset(), 2500)
+showHeader(2.7666)
+setTimeout(() => reset(), 1500)
