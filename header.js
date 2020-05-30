05029ac2e97ef066ff1888105655a5c1a3497bb2
Srdjan Strbanovic <srdjan@timok.com>
2020-05-30 15:19:34 -0400

    footer changes

--- header.js
+++ header.js
@@ -3,8 +3,4 @@ let cards = document.querySelectorAll(".card")
 const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
 const h = i => (i * 360) / cards.length
 const l = i => 100 - ((i + 1) * 100) / cards.length
-const showHeader = (i) => cards.forEach((c, j) => c.style.backgroundColor = hsl(h(i + j / 20), 100, l(j)))
-// const reset = () => cards.forEach((c, i) => c.style.backgroundColor = hsl(0, 0, l(i)))
-
-showHeader(2.8666)
-// setTimeout(() => reset(), 1500)
+cards.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + j / 20), 100, l(j)))
