dcb0a7f5fd86383de1aabcf561570379d22622e7
Srdjan Strbanovic <srdjan@timok.com>
2020-07-07 22:00:54 -0400

    setting the animation timing right

--- header.js
+++ header.js
@@ -5,5 +5,10 @@ let length = headers.length
 const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
 const h = i => (i * 360) / length
 const l = i => 100 - ((i + 1) * 100) / length
-headers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + j / 20), 100, l(j)))
-footers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + length-j / 20), 100, l(length-j)))
+
+let showHandF = function() {
+  setTimeout(headers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + j / 20), 100, l(j))), 500)
+  setTimeout(footers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + length-j / 20), 100, l(length-j))), 500)
+}
+
+setTimeout(showHandF, 500)
