44c18aa6c8cba724a48507c599e3e61a3369a7b1
Srdjan Strbanovic <srdjan@timok.com>
2020-07-08 21:39:36 -0400

    simplified header and footer animation

--- header.js
+++ header.js
@@ -7,8 +7,8 @@ const h = i => (i * 360) / length
 const l = i => 100 - ((i + 1) * 100) / length
 
 let showHandF = function() {
-  setTimeout(headers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + j / 20), 100, l(j))), 500)
-  setTimeout(footers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + length-j / 20), 100, l(length-j))), 500)
+  headers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + j / 20), 100, l(j)))
+  footers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + length-j / 20), 100, l(length-j)))
 }
 
 setTimeout(showHandF, 500)
