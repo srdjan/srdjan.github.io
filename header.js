b0b9632d365a0f2ff84db820377bcc6150cb58f0
Srdjan Strbanovic <srdjan@timok.com>
2020-08-05 00:13:40 -0400

    start animations sooner

--- header.js
+++ header.js
@@ -6,9 +6,21 @@ const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
 const h = i => (i * 360) / length
 const l = i => 100 - ((i + 1) * 100) / length
 
-let showHandF = function() {
-  headers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + j / 20), 100, l(j)))
-  footers.forEach((c, j) => c.style.backgroundColor = hsl(h(2.86 + length-j / 20), 100, l(length-j)))
-}
+setTimeout(
+  headers.forEach(
+    (c, j) => (c.style.backgroundColor = hsl(h(2.86 + j / 20), 100, l(j)))
+  ),
+  250
+)
 
-setTimeout(showHandF, 500)
+setTimeout(
+  footers.forEach(
+    (c, j) =>
+      (c.style.backgroundColor = hsl(
+        h(2.86 + length - j / 20),
+        100,
+        l(length - j)
+      ))
+  ),
+  1750
+)
