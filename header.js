8c7bd3953716ee48c52fec7e9072347450b150a9
srdjan <srdjan@timok.com>
2022-11-01 07:54:09 -0400

    removed .foot cleanup transitions

--- header.js
+++ header.js
@@ -1,21 +1,11 @@
 const headers = document.querySelectorAll('.head')
-const footers = document.querySelectorAll('.foot')
-const length = headers.length
 
 const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
-const h = i => (i * 360) / length
-const l = i => 100 - ((i + 1) * 100) / length
+const h = i => (i * 360) / headers.length
+const l = i => 100 - ((i + 1) * 100) / headers.length
 
 setTimeout(
   headers.forEach(
-    (c, j) => (c.style.backgroundColor = hsl(h(3), 90, l(j)))
-  ),
-  250
-)
-
-setTimeout(
-  footers.forEach(
-    (c, j) => (c.style.backgroundColor = hsl(h(3), 90, l(length - j)))
-  ),
-  1250
+    (c, j) => (c.style.backgroundColor = hsl(h(3), 70, l(j)))
+  ), 3000
 )
