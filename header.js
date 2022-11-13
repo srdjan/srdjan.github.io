f4129662397dfad879f2ff8d6ae8df540bb30221
Srdjan <srdjan@timok.com>
2022-11-13 09:06:03 -0500

    fixed header animation

--- header.js
+++ header.js
@@ -1,11 +1,18 @@
 const headers = document.querySelectorAll('.head')
+const footers = document.querySelectorAll('.foot')
+const length = headers.length
 
 const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
-const h = i => (i * 360) / headers.length
-const l = i => 100 - ((i + 1) * 100) / headers.length
+const h = i => (i * 360) / length
+const l = i => 100 - ((i + 1) * 100) / length
 
 setTimeout(
   headers.forEach(
-    (c, j) => (c.style.backgroundColor = hsl(h(3), 70, l(j)))
-  ), 3000
+    (c, j) => (c.style.backgroundColor = hsl(h(3), 90, l(j)))), 1250
+)
+
+setTimeout(
+  footers.forEach(
+    (c, j) => (c.style.backgroundColor = hsl(h(3), 90, l(length - j)))
+  ), 2250
 )
