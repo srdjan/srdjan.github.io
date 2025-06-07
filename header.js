2a57226db3444157fc71adcd1e09d79890b21aa0
srdjan <srdjan@timok.com>
2025-06-07 19:56:34 -0400

    refactored

--- header.js
+++ header.js
@@ -1,30 +1,22 @@
-const headers = document.querySelectorAll('.head')
-const footers = document.querySelectorAll('.foot')
-const length = headers.length
+const headers = document.querySelectorAll('.head');
+const length = headers.length;
 
-const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`
-const h = i => (i * 360) / length
-const l = i => 100 - ((i + 1) * 100) / length
+if (length === 0) {
+  console.warn('No header elements found for animation');
+}
 
-setTimeout(
-  headers.forEach(
-    (c, j) => (c.style.backgroundColor = hsl(h(3), 90, l(j)))), 1250
-)
+const hsl = (h, s, l) => `hsl(${h}deg, ${s}%, ${l}%)`;
+const h = i => (i * 360) / length;
+const l = i => 100 - ((i + 1) * 100) / length;
 
-setTimeout(
-  footers.forEach(
-    (c, j) => (c.style.backgroundColor = hsl(h(3), 90, l(length - j)))
-  ), 2250
-)
+// Use requestAnimationFrame for better performance
+const animateHeaders = () => {
+  headers.forEach((element, index) => {
+    element.style.backgroundColor = hsl(h(3), 90, l(index));
+  });
+};
 
-// const isMobile = ('ontouchstart' in document.documentElement && navigator.userAgent.match(/Mobi/))
-// if (!isMobile) {
-//   console.log('Not mobile device')
-// }
-// else {
-//   target.addEventListener('touchstart', function (e) {
-//     const avatar = document.getElementsByClassName('avatar')
-//     avatar.backgroundColor = '#ffff'
-//     e.preventDefault();
-//   })
-// }
+// Delay animation start
+setTimeout(() => {
+  requestAnimationFrame(animateHeaders);
+}, 1250);
